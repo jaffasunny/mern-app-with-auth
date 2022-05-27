982d57b8c82c2990ed3356de677405618476ba4e
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-05-27 15:49:53 -0700

    Login route and Hashed password

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,18 @@ const userSchema = new mongoose.Schema({
     type: "String",
     required: true,
   },
+});
+
+// we are hashing the password
+// we use normal function because of this keyword
+userSchema.pre("save", async function (next) {
+  console.log("hello im pre save function");
+  // This only works when password changes
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
+  }
+  next();
 });
 
 // After defining user schema
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,48 @@ router.get("/", (req, res) => {
   res.send("Hello from server routerjs");
 });
 
-router.post("/register", (req, res) => {
+// This was made using old Promise approach
+
+// router.post("/register", (req, res) => {
+//   // getting data from user req.body
+//   const { name, email, phone, work, password, cpassword } = req.body;
+
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(422).json({ error: "Please fill all fields properly" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({
+//           error:
+//             "User is already registered, please login with your proper credentials!",
+//         });
+//       }
+
+//       const newUser = new User({
+//         name,
+//         email,
+//         phone,
+//         work,
+//         password,
+//         cpassword,
+//       });
+
+//       newUser
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "User is registered successfully!" });
+//         })
+//         .catch((err) => {
+//           res.status(500).json({ error: `Failed to register ${err.message}` });
+//         });
+//     })
+//     .catch((error) => console.log(error.message));
+// });
+
+// This is made using async await approach
+router.post("/register", async (req, res) => {
   // getting data from user req.body
   const { name, email, phone, work, password, cpassword } = req.body;
 
@@ -16,34 +57,56 @@ router.post("/register", (req, res) => {
     return res.status(422).json({ error: "Please fill all fields properly" });
   }
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({
-          error:
-            "User is already registered, please login with your proper credentials!",
-        });
-      }
-
-      const newUser = new User({
-        name,
-        email,
-        phone,
-        work,
-        password,
-        cpassword,
+  // This returns promise
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({
+        error:
+          "User is already registered, please login with your proper credentials!",
       });
+    } else if (password != cpassword) {
+      return res.status(422).json({
+        error: "Pasword are not matching",
+      });
+    }
 
-      newUser
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "User is registered successfully!" });
-        })
-        .catch((err) => {
-          res.status(500).json({ error: `Failed to register ${err.message}` });
-        });
-    })
-    .catch((error) => console.log(error.message));
+    const newUser = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      cpassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User is registered successfully!" });
+  } catch (err) {
+    res.send({ error: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    res.status(422).send({ error: "Please fill all fields properly!" });
+  } else if (!email || !password) {
+    res.status(400).send({ error: "Please fill all details!" });
+  }
+
+  try {
+    const user = await User.findOne({ email: email, password: password });
+    if (user) {
+      return res.json({ message: "You are logged in successfully" });
+    } else {
+      return res.json({ error: "User not found" });
+    }
+  } catch (err) {
+    console.log({ error: err });
+  }
 });
 
 module.exports = router;
